276da1afa3be1547f90e61a64952f5404c46447c
larissanakazato <larissa_nakazato@hotmail.com>
2024-10-19 14:54:00 -0300

    iniciando o projeto e implementando a entidade serviço na camada modelo

---
+++ Model/Servico.js
@@ -0,0 +1,79 @@
+export  default class Servico{
+    #id
+    #nome
+    #descricao
+    #valor
+    #urlImagem
+
+    //SLA
+    #tempoInicioAtendimento
+    #tempoSolucao
+
+    get id(){
+        return this.#id;
+    }
+    set id(novoId){
+        this.#id=novoId;
+    }
+
+    get nome(){
+        return this.#nome;
+    }
+    set nome(novoNome){
+        this.#nome=novoNome;
+    }
+
+    get descricao(){
+        return this.#descricao;
+    }
+    set descricao(novoDescricao){
+        this.#descricao=novoDescricao;
+    }
+
+    get valor(){
+        return this.#valor;
+    }
+    set valor(novoValor){
+        this.#valor=novoValor;
+    }
+
+    get urlImagem(){
+        return this.#urlImagem;
+    }
+    set urlImagem(novoUrl){
+        this.#urlImagem=novoUrl;
+    }
+
+    get tempoInicioAtendimento(){
+        return this.#tempoInicioAtendimento;
+    }
+    set tempoInicioAtendimento(novoTempo){
+        this.#tempoInicioAtendimento=novoTempo;
+    }
+
+    get tempoSolucao(){
+        return this.#tempoSolucao;
+    }
+    set tempoSolucao(novoTempo){
+        this.#tempoSolucao=novoTempo;
+    }
+
+    //override
+    toJSON(){
+        return {
+            id:this.#id,
+            nome:this.#nome,
+            descricao:this.#descricao,
+            valor:this.#valor,
+            urlImagem:this.#urlImagem,
+            tempoInicioAtendimento:this.#tempoInicioAtendimento,
+            tempoSolucao:this.#tempoSolucao
+        }
+    }
+
+    async gravar(){}
+    async alterar(){}
+    async excluir(){}
+    async consultar(){}
+
+}
